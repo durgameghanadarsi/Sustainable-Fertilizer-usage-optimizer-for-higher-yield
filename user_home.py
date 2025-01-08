2568075dcdeb6981afa6f9e681e2a88e46c207de
durgameghanadarsi <113083539+durgameghanadarsi@users.noreply.github.com>
2025-01-08 12:21:39 +0530

    Update user_home.py

--- user_home.py
+++ user_home.py
@@ -96,9 +96,7 @@ def fertilizer():
     location=user[3]
     a=temp.split('°')[0]
     humd=other_data.find('°')
-    b=other_data[humd-3:humd]
-    if b=='':
-        b=float(64.57)
+    b=float(64.57)
     col1,col2,col3= st.columns([5,5,5])
     with col1:
         c=st.number_input('Moisture',min_value=25.0,max_value=100.0)
