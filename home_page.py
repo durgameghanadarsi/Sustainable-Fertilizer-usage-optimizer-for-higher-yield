906468a9c8177384d297eb70d2284f15b2287b50
durgameghanadarsi <113083539+durgameghanadarsi@users.noreply.github.com>
2025-01-08 11:27:20 +0530

    Update home_page.py

--- home_page.py
+++ home_page.py
@@ -3,18 +3,32 @@ import streamlit as st
 def home_page():
     st.markdown(
         """
+        <style>
+        /* Apply background image to the main content area */
+        .main {
+            background-image: url('https://images.rawpixel.com/image_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvdjkzOS1udW5ueS0wMi5qcGc.jpg');
+            background-size: cover;
+            background-position: center;
+            background-repeat: no-repeat;
+            min-height: 100vh;  /* Ensure the background covers the whole screen */
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+    st.markdown(
+        """
         <div style="text-align: center;">
             <h1>Sustainable Fertilizer Usage Optimizer for Higher Yield</h1>
         </div>
         """,
         unsafe_allow_html=True
     )
-
     # Center the image
     st.markdown(
         """
         <div style="text-align: center;">
-            <img src="https://media2.giphy.com/media/IfsxP5r7WmhP5P6seN/giphy.gif?cid=6c09b952o7a4fgdk41h4cbnu9cdl00zq5fyej4d5yslelutf&ep=v1_gifs_search&rid=giphy.gif&ct=g" style="max-width: 100%;">
+            <img src="https://assets-v2.lottiefiles.com/a/43aff328-ee85-11ee-b1b3-43aa48f22991/Kk6fZSgixc.png", width="500">
         </div>
         """,
         unsafe_allow_html=True
