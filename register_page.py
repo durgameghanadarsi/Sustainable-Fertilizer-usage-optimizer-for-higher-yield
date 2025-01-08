3267c904330da3ebfe61ba5e47b9aeec45ecfc34
durgameghanadarsi <113083539+durgameghanadarsi@users.noreply.github.com>
2025-01-08 11:28:11 +0530

    Update register_page.py

--- register_page.py
+++ register_page.py
@@ -1,18 +1,48 @@
 import streamlit as st
 import re
 from db_manager import register_user
+import requests
+from bs4 import BeautifulSoup
 
 def register_page():
+    def valid_location(city):
+        url = "https://www.google.com/search?q=" + "weather" + city
+        html = requests.get(url).content
+
+        # Getting raw data using BeautifulSoup
+        soup = BeautifulSoup(html, 'html.parser')
+        if soup.find('div', attrs={'class': 'BNeawe iBp4i AP7Wnd'}) is None:
+            return False
+        return True
+
+    st.markdown(
+        """
+        <style>
+        /* Apply background image to the main content area */
+        .main {
+            background-image: url('https://t4.ftcdn.net/jpg/03/11/73/01/360_F_311730118_ezoi92RU1WvSk4hRDkS4R7r4f4wg6MWb.webp');
+            background-size: cover;
+            background-position: center;
+            background-repeat: no-repeat;
+            min-height: 100vh;  /* Ensure the background covers the whole screen */
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
     # Center the registration form container using Streamlit form layout
     with st.form(key="register_form"):
         # Title
-        st.title("Register Page")
+        st.title("Sign Up🔐")
 
         # Form Fields
         name = st.text_input("Name")
-        email = st.text_input("Email")
-        password = st.text_input("Password", type="password")
-        retype_password = st.text_input("Retype Password", type="password")
+        col1,col2=st.columns(2)
+        email = col1.text_input("Email")
+        location = col2.text_input("Location📍")
+        col1, col2 = st.columns(2)
+        password = col1.text_input("Password", type="password")
+        retype_password = col2.text_input("Retype Password", type="password")
 
         # Submit Button inside the form
         register_button = st.form_submit_button("Register")
@@ -27,8 +57,24 @@ def register_page():
                 st.error("Password must be at least 6 characters long!")
             elif password != retype_password:
                 st.error("Passwords do not match!")
+            elif not valid_location(location):
+                st.error("Invalid Location!")
             else:
-                if register_user(name, email, password):
-                    st.success("Registration Successful!")
+                if register_user(name, email,location, password):
+                    st.markdown(
+                        """
+                        <div style="text-align: center; padding: 1px; background-color: green; border-radius: 1px; border: 1.5px solid black; margin-bottom: 20px;">
+                            <p style="color: black; font-size: 20px;"><b>Registration Successful! Please login to continue.</b></p>
+                        </div>
+                        """,
+                        unsafe_allow_html=True
+                    )
                 else:
-                    st.error("Email already exists!")
+                    st.markdown(
+                        """
+                        <div style="text-align: center; padding: 1px; background-color: red; border-radius: 1px; border: 1.5px solid black; margin-bottom: 20px;">
+                            <p style="color: black; font-size: 20px;"><b>Registration Failed! Email already exists.</b></p>
+                        </div>
+                        """,
+                        unsafe_allow_html=True
+                    )
